c4978c67099259693733860a21384cef7ed8e830
Bruce Borrett <oceativa@gmail.com>
2023-11-28 12:47:49 +0200

    Updated project selector component.

--- ui/src/app/components/project-selector/project-selector.component.ts
+++ ui/src/app/components/project-selector/project-selector.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
+import { KanbanService } from '../../services/kanban.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-selector',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor],
   templateUrl: './project-selector.component.html',
   styleUrl: './project-selector.component.css'
 })
 export class ProjectSelectorComponent {
+  constructor(private kanbanService: KanbanService, private router: Router) { }
 
+  projects = this.kanbanService.projects();
+
+  changeProject(event: Event) {
+    const projectId = (event.target as HTMLSelectElement).value;
+    this.router.navigate([`/project/${projectId}`]);
+  }
 }
