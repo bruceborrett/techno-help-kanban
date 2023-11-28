90c1d0efa3a53c4fd1f068f3bf9ebe4fcee7e8ab
Bruce Borrett <oceativa@gmail.com>
2023-11-28 15:45:44 +0200

    Updated profile page.

--- ui/src/app/pages/profile/profile.component.ts
+++ ui/src/app/pages/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AvatarComponent } from '../../components/avatar/avatar.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,  AvatarComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
